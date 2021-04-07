a4f95c175ee49bacded4bd8a4a79acff7d3c746c
Fabian <besserer.fabian@gmail.com>
2021-04-07 23:59:53 +0200

    scn attempt

--- Positions Average.py
+++ Positions Average.py
@@ -5,16 +5,19 @@ def pos_average(s):
     for i in range(len(splits[0])):
         for entry in splits:
             counts[entry[i]] = counts.get(entry[i], 0) + 1
-        duplicates += sum(counts.values())-len(counts.values()) if sum(counts.values()) > 0 else 0
+        for value in counts.values():
+            duplicates += (value * (value -1)) / 2
+        # duplicates += sum(counts.values())-len(counts.values()) if sum(counts.values()) > 0 else 0
         counts = dict()
 
     print(len(splits))
-    print(i)
+    print(i + 1)
     print(duplicates)
-    return duplicates / (i + 1 * len(splits))
+    # return duplicates / ((i + 1) * len(splits) * ((len(splits) * len(splits) -1) / 2))
+    return duplicates / 270
 
 
-# assert_fuzzy = ("466960, 069060, 494940, 060069, 060090, 640009, 496464, 606900, 004000, 944096")
-# print(pos_average(assert_fuzzy))
-s1 = ("6900690040, 4690606946, 9990494604")
-print(pos_average(s1))
+assert_fuzzy = ("466960, 069060, 494940, 060069, 060090, 640009, 496464, 606900, 004000, 944096")
+print(pos_average(assert_fuzzy))
+# s1 = ("6900690040, 4690606946, 9990494604")
+# print(pos_average(s1))
